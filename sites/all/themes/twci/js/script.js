08c42fdc28ad2764969f371419f42944df347504
cqbent <cqbent@gmail.com>
2014-12-15 12:24:18 -0500

    initial site commit

---
+++ sites/all/themes/twci/js/script.js
@@ -0,0 +1,39 @@
+/**
+ * @file
+ * A JavaScript file for the theme.
+ *
+ * In order for this JavaScript to be loaded on pages, see the instructions in
+ * the README.txt next to this file.
+ */
+
+// JavaScript should be made compatible with libraries other than jQuery by
+// wrapping it with an "anonymous closure". See:
+// - https://drupal.org/node/1446420
+// - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
+(function ($, Drupal, window, document, undefined) {
+
+
+// To understand behaviors, see https://drupal.org/node/756722#behaviors
+Drupal.behaviors.my_custom_behavior = {
+    attach: function(context, settings) {
+    
+    // consultation form  
+    $('#block-entityform-block-schedule-consultation .box-lower, #block-entityform-block-schedule-consultation .block__title').click(function() {
+        // if form elements hidden then hide; otherwise show; change box-lower status
+        if ($('#block-entityform-block-schedule-consultation').hasClass('active')) {
+            $('#block-entityform-block-schedule-consultation').removeClass('active');
+        }
+        else {
+            $('#block-entityform-block-schedule-consultation').addClass('active');
+        }        
+    });
+    
+    // home page: get height of window and set content to height
+    var window_height = $(window).height();
+    $('.front #page #content').css('height', window_height-150);
+
+  }
+};
+
+
+})(jQuery, Drupal, this, this.document);
---
+++ sites/all/themes/twci/template.php
@@ -0,0 +1,165 @@
+<?php
+/**
+ * @file
+ * Contains the theme's functions to manipulate Drupal's default markup.
+ *
+ * Complete documentation for this file is available online.
+ * @see https://drupal.org/node/1728096
+ */
+
+
+/**
+ * Override or insert variables into the maintenance page template.
+ *
+ * @param $variables
+ *   An array of variables to pass to the theme template.
+ * @param $hook
+ *   The name of the template being rendered ("maintenance_page" in this case.)
+ */
+/* -- Delete this line if you want to use this function
+function twci_preprocess_maintenance_page(&$variables, $hook) {
+  // When a variable is manipulated or added in preprocess_html or
+  // preprocess_page, that same work is probably needed for the maintenance page
+  // as well, so we can just re-use those functions to do that work here.
+  twci_preprocess_html($variables, $hook);
+  twci_preprocess_page($variables, $hook);
+}
+// */
+
+/**
+ * Override or insert variables into the html templates.
+ *
+ * @param $variables
+ *   An array of variables to pass to the theme template.
+ * @param $hook
+ *   The name of the template being rendered ("html" in this case.)
+ */
+/* -- Delete this line if you want to use this function
+function twci_preprocess_html(&$variables, $hook) {
+  $variables['sample_variable'] = t('Lorem ipsum.');
+
+  // The body tag's classes are controlled by the $classes_array variable. To
+  // remove a class from $classes_array, use array_diff().
+  //$variables['classes_array'] = array_diff($variables['classes_array'], array('class-to-remove'));
+}
+// */
+
+/**
+ * Override or insert variables into the page templates.
+ *
+ * @param $variables
+ *   An array of variables to pass to the theme template.
+ * @param $hook
+ *   The name of the template being rendered ("page" in this case.)
+ */
+function twci_preprocess_page(&$variables, $hook) {
+    drupal_add_js('//use.typekit.net/aur4mpg.js', 'external');
+    drupal_add_js('try{Typekit.load();}catch(e){}', 'inline');
+    drupal_add_css('//cloud.typography.com/7299932/685246/css/fonts.css');
+}
+// */
+
+/**
+ * Override or insert variables into the node templates.
+ *
+ * @param $variables
+ *   An array of variables to pass to the theme template.
+ * @param $hook
+ *   The name of the template being rendered ("node" in this case.)
+ */
+/* -- Delete this line if you want to use this function
+function twci_preprocess_node(&$variables, $hook) {
+  $variables['sample_variable'] = t('Lorem ipsum.');
+
+  // Optionally, run node-type-specific preprocess functions, like
+  // twci_preprocess_node_page() or twci_preprocess_node_story().
+  $function = __FUNCTION__ . '_' . $variables['node']->type;
+  if (function_exists($function)) {
+    $function($variables, $hook);
+  }
+}
+// */
+
+/**
+ * Override or insert variables into the comment templates.
+ *
+ * @param $variables
+ *   An array of variables to pass to the theme template.
+ * @param $hook
+ *   The name of the template being rendered ("comment" in this case.)
+ */
+/* -- Delete this line if you want to use this function
+function twci_preprocess_comment(&$variables, $hook) {
+  $variables['sample_variable'] = t('Lorem ipsum.');
+}
+// */
+
+/**
+ * Override or insert variables into the region templates.
+ *
+ * @param $variables
+ *   An array of variables to pass to the theme template.
+ * @param $hook
+ *   The name of the template being rendered ("region" in this case.)
+ */
+/* -- Delete this line if you want to use this function
+function twci_preprocess_region(&$variables, $hook) {
+  // Don't use Zen's region--sidebar.tpl.php template for sidebars.
+  //if (strpos($variables['region'], 'sidebar_') === 0) {
+  //  $variables['theme_hook_suggestions'] = array_diff($variables['theme_hook_suggestions'], array('region__sidebar'));
+  //}
+}
+// */
+
+/**
+ * Override or insert variables into the block templates.
+ *
+ * @param $variables
+ *   An array of variables to pass to the theme template.
+ * @param $hook
+ *   The name of the template being rendered ("block" in this case.)
+ */
+/* -- Delete this line if you want to use this function
+function twci_preprocess_block(&$variables, $hook) {
+  // Add a count to all the blocks in the region.
+  // $variables['classes_array'][] = 'count-' . $variables['block_id'];
+
+  // By default, Zen will use the block--no-wrapper.tpl.php for the main
+  // content. This optional bit of code undoes that:
+  //if ($variables['block_html_id'] == 'block-system-main') {
+  //  $variables['theme_hook_suggestions'] = array_diff($variables['theme_hook_suggestions'], array('block__no_wrapper'));
+  //}
+}
+// */
+
+// alter consultation form
+function twci_form_alter(&$form, &$form_state, $form_id) {
+    // form_id is consultation then add wrapper to form elements area
+    if ($form_id == 'schedule_consultation_entityform_edit_form') {
+        //dsm($form['#attributes']);
+        //$form['#attributes']['class'][] = 'xxx-class';
+    }
+}
+
+// add form-select-wrapper span class around select element; change "None" to element title
+function twci_select($variables) {
+    //dsm($variables);
+    $element = $variables['element'];
+    element_set_attributes($element, array('id', 'name', 'size'));
+    _form_set_class($element, array('form-select'));
+    if (isset($element['#options']['_none'])) {
+        $element['#options']['_none'] = $element['#title'];
+    }
+    return '<span class="form-select-wrapper"><select' . drupal_attributes($element['#attributes']) . '>' . form_select_options($element) . '</select></span>';
+}
+
+// display background images from page node
+function display_bk_image($node) {
+    // if node has background image then get background image and display it 
+    if (isset($node->field_background_image['und'][0])) {
+        $bkimg = field_view_field('node', $node, 'field_background_image', array('label' => 'hidden'));
+        //dsm($bkimg);
+        print render($bkimg);
+    }
+    
+}
